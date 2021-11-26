6210c10295c284001136a6891593b4c07dcd937c
Михаил Гришин <mikhail0910grishin@gmail.com>
2021-11-26 20:54:58 +0300

    Created main class "MyTelegramBot"

--- src/main/java/com/example/telegrambot/MyTelegramBot.java
+++ src/main/java/com/example/telegrambot/MyTelegramBot.java
@@ -1,4 +1,37 @@
 package com.example.telegrambot;
 
-public class MyTelegramBot {
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MyTelegramBot extends TelegramLongPollingBot {
+    private String botUserName;
+    private String botToken;
+
+    public MyTelegramBot(DefaultBotOptions botOptions){
+        super(botOptions);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUserName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public void setBotUserName(String botUserName) {
+        this.botUserName = botUserName;
+    }
+
+    public void setBotToken(String botToken) {
+        this.botToken = botToken;
+    }
 }
