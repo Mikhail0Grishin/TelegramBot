778075c6bb3f905657e9800467d3102523d2906c
Михаил Гришин <mikhail0910grishin@gmail.com>
2021-11-30 21:05:04 +0300

    Created keyboard

--- src/main/java/com/home/telegrambot/MyTelegramBot.java
+++ src/main/java/com/home/telegrambot/MyTelegramBot.java
@@ -8,8 +8,11 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 public class MyTelegramBot extends TelegramLongPollingBot {
@@ -90,7 +93,38 @@ public class MyTelegramBot extends TelegramLongPollingBot {
     }
 
     private InlineKeyboardMarkup getKeyboard() {
-        return null;
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+
+        InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+        inlineKeyboardButton.setText(INFO_LABEL);
+        inlineKeyboardButton.setCallbackData(COMMANDS.INFO.getCommand());
+
+        InlineKeyboardButton inlineKeyboardButtonSearch = new InlineKeyboardButton();
+        inlineKeyboardButtonSearch.setText(SEARCH_LABEL);
+        inlineKeyboardButtonSearch.setCallbackData(COMMANDS.SEARCH.getCommand());
+
+        InlineKeyboardButton inlineKeyboardButtonSubscribe = new InlineKeyboardButton();
+        inlineKeyboardButtonSubscribe.setText(SUBSCRIBE_LABEL);
+        inlineKeyboardButtonSubscribe.setCallbackData(COMMANDS.SUBSCRIBE.getCommand());
+
+        List<List<InlineKeyboardButton>> keyboardButtons = new ArrayList<>();
+
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        keyboardButtonsRow1.add(inlineKeyboardButton);
+
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+        keyboardButtonsRow2.add(inlineKeyboardButtonSearch);
+
+        List<InlineKeyboardButton> keyboardButtonsRow3 = new ArrayList<>();
+        keyboardButtonsRow3.add(inlineKeyboardButtonSubscribe);
+
+        keyboardButtons.add(keyboardButtonsRow1);
+        keyboardButtons.add(keyboardButtonsRow2);
+        keyboardButtons.add(keyboardButtonsRow3);
+
+        inlineKeyboardMarkup.setKeyboard(keyboardButtons);
+
+        return inlineKeyboardMarkup;
     }
 
     private SendMessage getCommandResponse(String text, User from, String valueOf) {
